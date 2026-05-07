aaef358ca27b2204af0659b13d88db51632fb1e0
mohsen-alihosseini <en.alihosseini@gmail.com>
2026-05-07 23:51:15 +0200

    2026 05 07 generator code edited to be more readble and comment added

--- Exercise_0/src_to_implement/main.py
+++ Exercise_0/src_to_implement/main.py
@@ -2,41 +2,28 @@ import pattern
 import generator
 import matplotlib.pyplot as plt
 
-### Checker debugging
-a = pattern.Checker(80,10)
-cc = a.draw()
-a.show()
-print(cc.shape)
-c = pattern.Checker(500, 1)
-res = c.draw()
-res[:] = 0
-print(res)
-print(c.output)
+#_________Checkboard
+# Checkerboard=pattern.Checker(200,10)
+# Checkerboard.draw()
+# Checkerboard.show()
 
 
-### Cirlce debugging
-a = pattern.Circle(500,100,(250,250))
-a.draw()
-a.show()
+#_________Cirlce
+# circle = pattern.Circle(500,100,(250,250))
+# circle.draw()
+# circle.show()
 
+#_________Spectrum
+# spect = pattern.Spectrum(100)
+# spect.draw()
+# spect.show()
 
-### Spectrum debugging
-s = pattern.Spectrum(100)
-s.draw()
-s.show()
-
-
-### Generator debugging
-g = generator.ImageGenerator('./exercise_data/', './Labels.json', 3, [64, 64, 3], rotation=False, mirroring=False, shuffle=False)
+#_________Generator
+g = generator.ImageGenerator('./exercise_data/', './Labels.json', 10, [128, 128, 3], rotation=True, mirroring=False, shuffle=False)
 
 g.next()
-
-
 g.show()
-
-
-# images, labels = g.next()
-# for i,j in enumerate(images):
-#     plt.imshow(j)
-#     print(labels[i])
-#     plt.show()
+print(g.batch_number)
+# g.next()
+# g.show()
+# print(g.batch_number)
--- Exercise_0/src_to_implement/pattern.py
+++ Exercise_0/src_to_implement/pattern.py
@@ -3,19 +3,23 @@ import matplotlib.pyplot as plt
 
 class Checker():
     def __init__(self, resolution, tile_size):
+
         self.resolution = resolution
         self.tile_size = tile_size
-        self.output = np.zeros((resolution,resolution))
-        # print(self.output.shape)
+
+        
 
     def draw(self):
         if self.resolution%(2*self.tile_size)==0 :
             # print("drawing")
-            temp = np.concatenate((np.zeros((self.tile_size,self.tile_size)), np.ones((self.tile_size,self.tile_size))), axis=1)
-            temp = np.vstack((temp,1-temp))
-            self.output = np.tile(temp,(int(((self.resolution/self.tile_size))/2), int(((self.resolution/self.tile_size))/2)))
-            # self.output = temp.reshape((self.resolution,self.resolution))
+            a_ = np.concatenate((np.zeros((self.tile_size,self.tile_size)), np.ones((self.tile_size,self.tile_size))), axis=1)
+            b_ = 1-a_ #reverse the tiles
+            tmp = np.concatenate((a_,b_),axis=0)
+            reps = int(self.resolution / (2 * self.tile_size))
+            self.output = np.tile(tmp,(reps,reps))
             # print(self.output)
+        else:
+            print("Givenvalue for Resulution is not dividable by 2")
         return np.array(self.output,copy=True)
 
     def show(self):
@@ -28,13 +32,18 @@ class Circle():
         self.resolution = resolution
         self.radius = radius
         self.position = position
-        # self.output = np.ones((self.resolution, self.resolution))
+        
 
     def draw(self):
-        x, y = np.meshgrid(np.arange(self.resolution), np.arange(self.resolution))
-        x0,y0 = self.position
+        x= np.arange(self.resolution)
+        y= np.arange(self.resolution)
+        x,y=np.meshgrid(x,y)
+        # print(self.position)
+        c_x,c_y = self.position #circle center
         # print(x,y)
-        self.output = np.where((x-x0)**2+(y-y0)**2<self.radius**2,True,False)
+        self.output = np.zeros((self.resolution,self.resolution)) ##all zeroo
+        self.output[(x-c_x)**2+(y-c_y)**2<self.radius**2]=1      ## all zero except where condition true
+        # print(self.output)
         return np.array(self.output, copy=True)
 
     def show(self):
@@ -47,46 +56,32 @@ class Spectrum():
         self.resolution = resolution
         self.output = np.zeros((self.resolution, self.resolution,3))
 
-    def draw(self):
-        self.output[:,:,0] = np.tile(np.linspace(0,1,self.resolution),self.resolution).reshape((self.resolution,self.resolution))
-        self.output[:,:,2] = np.tile(np.linspace(1,0,self.resolution),self.resolution).reshape((self.resolution,self.resolution))
-        self.output[:,:,1] = np.tile(np.linspace(0,1,self.resolution).reshape(-1,1),self.resolution)
-        return np.array(self.output, copy=True)
-        # x, y = np.meshgrid(np.arange(self.resolution), np.arange(self.resolution))
-        # self.output[:,:,2] = 1 - (x**2 + y**2)/(2*(self.resolution-1)**2)
-        # self.output[:,:,1] = 1 - ((x-(self.resolution-1)/2)**2 + (y-(self.resolution-1))**2)/(2*(self.resolution-1)**2)
-        # self.output[:,:,0] = 1 - ((x-(self.resolution-1))**2 + y**2)/(2*(self.resolution-1)**2)
-        
-        # self.output[:,:,0] = 1 - np.sqrt((x**2 + y**2)/(2*(self.resolution-1)**2))
-        # self.output[:,:,1] = 1 - np.sqrt(((x-(self.resolution-1)/2)**2 + (y-(self.resolution-1))**2)/(2*(self.resolution-1)**2))
-        # self.output[:,:,2] = 1 - np.sqrt(((x-(self.resolution-1))**2 + y**2)/(2*(self.resolution-1)**2))
 
-        # self.output[:,:,2] = 1 - (x + y)/(self.resolution-1)
-        # self.output[:,:,1] = 1 - (abs(x-(self.resolution-1)/2) + abs(y-(self.resolution-1)))/(self.resolution-1)
-        # self.output[:,:,0] = 1 - (abs(x-(self.resolution-1)) + y)/(self.resolution-1)
-        # self.output = np.where(self.output<0,0,self.output)
-        # print(self.output[:,:,0])
+    # Top-Left (Blue): Red=0, Green=0, Blue=1
+    # Top-Right (Red): Red=1, Green=0, Blue=0
+    # Bottom-Left (Cyan): Red=0, Green=1, Blue=1
+    # Bottom-Right (Yellow): Red=1, Green=1, Blue=0
+
+    # Red increases from left to right (x-axis)
+    # Green increases from top to bottom (y-axis)
+    # Blue decreases from left to right (x-axis).
+    
+    def draw(self):
+        r_line= np.tile(np.linspace(0,1,self.resolution),self.resolution)
+        self.output[:,:,0]=r_line.reshape((self.resolution,self.resolution))
+
+        b_line= np.tile(np.linspace(1,0,self.resolution),self.resolution)
+        self.output[:,:,2] =b_line.reshape((self.resolution,self.resolution))
+
+        g_line= np.linspace(0,1,self.resolution).reshape(-1,1)
+        g_line= np.tile(g_line,self.resolution)        
+        self.output[:,:,1] = g_line
+
+        return np.array(self.output, copy=True)
+
 
     def show(self):
-        # plt.imshow(self.output[:,:,1], cmap='gray')
         plt.imshow(self.output)
         plt.show()
 
 
-# s = Spectrum(100)
-# s.draw()
-# s.show()
-
-# a = Circle(100,25,(40,60))
-# a.draw()
-# a.show()
-
-# a = Checker(80,10)
-# cc = a.draw()
-# a.show()
-# print(cc.shape)
-# c = Checker(100, 1)
-# res = c.draw()
-# res[:] = 0
-# print(res)
-# print(c.output)
