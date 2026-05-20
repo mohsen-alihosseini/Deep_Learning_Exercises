ce6a465856a1cab19e5bf9f2c63541c0a3f188fb
mohsen-alihosseini <en.alihosseini@gmail.com>
2026-05-20 15:36:02 +0200

    submission of exercise 1

---
+++ Exercise_1/src_to_implement/Layers/ReLU.py
@@ -0,0 +1,15 @@
+import numpy as np
+import Layers.Base as Base
+
+
+class ReLU(Base.BaseLayer):
+    def __init__(self):
+        super().__init__()
+        self.trainable = False
+
+    def forward(self, input_tensor):
+        self.input = input_tensor
+        return np.where(self.input > 0, self.input, 0)
+    
+    def backward(self, error_tensor):
+        return np.multiply(np.where(self.input > 0, 1, 0), error_tensor)
---
+++ Exercise_1/src_to_implement/Optimization/Optimizers.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+class Sgd():
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        update_weight = weight_tensor - self.learning_rate * gradient_tensor
+        return update_weight
